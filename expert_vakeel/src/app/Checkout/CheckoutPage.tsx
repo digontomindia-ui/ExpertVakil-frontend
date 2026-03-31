56336f84b33d3e2e8f4e059c5d933160546818ca
Milan721166 <milansahoo7211662005@gmail.com>
2026-03-31 11:30:08 +0530

    design chnge

--- expert_vakeel/src/app/Checkout/CheckoutPage.tsx
+++ expert_vakeel/src/app/Checkout/CheckoutPage.tsx
@@ -88,6 +88,7 @@ export default function CheckoutPage() {
                 >
                     Back to Challan Status
                 </button>
+                
             </div>
         );
     }
